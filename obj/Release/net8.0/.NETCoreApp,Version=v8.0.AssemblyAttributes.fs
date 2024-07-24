02acda4b7118b09f5dccd260430a6baea6d0917c
David Raab <davidraab83@gmail.com>
2024-07-25 01:16:59 +0200

    Removed bin/, obj/ from Repository

--- obj/Release/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETCoreApp,Version=v8.0", FrameworkDisplayName=".NET 8.0")>]
-                do ()
